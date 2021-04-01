d7120a35586b0f144ed9b3dc3a9d2440e2d8b592
Bart Weil <43183347+Bart-Weil@users.noreply.github.com>
2021-04-01 01:54:18 +0100

    Create Config.py
    Incomplete Config file which currently only holds parameter distribution constants used in initialisation.

---
+++ Config.py
@@ -0,0 +1,12 @@
+#Init Parameters
+    #Fully Connected Layer
+InitBiasDistType = 'gauss'
+InitBiasStDev = 10
+InitBiasMean = 0
+
+InitWeightDistType = 'gauss'
+InitWeightStDev = 10
+InitWeightMean = 0
+
+InitActivation = 'sigmoid'
+InitAggregation = 'sum'
